bb065a6132f01f406535c19316f043947ac70358
Christoffer <christoffer.andersson.dev@gmail.com>
2025-11-16 01:25:41 +0100

    changed incrementing dll version with flipflop

--- engine/src/dll.rs
+++ engine/src/dll.rs
@@ -25,7 +25,7 @@ fn format_dll_input() -> PathBuf {
     path
 }
 
-fn format_dll_output(i: usize) -> PathBuf {
+fn format_dll_output(flipflop: bool) -> PathBuf {
     let current_exe = env::current_exe().expect("could not get current exe path");
     let folder_path = current_exe
         .parent()
@@ -37,16 +37,17 @@ fn format_dll_output(i: usize) -> PathBuf {
         .to_str()
         .expect("could not convert os string to &str")
         .replace("-", "_");
-    let file_name = format!("{file_name}_{i}");
+    let file_version = if flipflop { 0 } else { 1 };
+    let file_name = format!("{file_name}_{file_version}");
 
     PathBuf::new()
         .join(folder_path)
         .join(dlopen::utils::platform_file_name(file_name))
 }
 
-fn load_dll<T>(i: usize) -> Container<DllApi<T>> {
+fn load_dll<T>(flipflop: bool) -> Container<DllApi<T>> {
     let input = format_dll_input();
-    let output = format_dll_output(i);
+    let output = format_dll_output(flipflop);
     println!("input {input:?}");
     println!("output {output:?}");
     fs::copy(input, &output).expect("could not copy dll");
@@ -71,7 +72,7 @@ pub struct DllCallbacks<T> {
     pub dll_watcher: notify::RecommendedWatcher,
     pub dll_change_channel: mpsc::Receiver<Result<notify::Event, notify::Error>>,
 
-    pub i: usize,
+    pub flipflop: bool,
 }
 
 impl<T> crate::Callbacks for DllCallbacks<T> {
@@ -96,8 +97,8 @@ impl<T> DllCallbacks<T> {
     // NOTE: the callbacks are not used
     // since they will be loaded from the DLL
     pub fn new(_callbacks: T) -> Self {
-        let i = 0;
-        let dll: Container<DllApi<T>> = load_dll(i);
+        let flipflop = true;
+        let dll: Container<DllApi<T>> = load_dll(flipflop);
 
         let callbacks = dll.new();
 
@@ -113,7 +114,7 @@ impl<T> DllCallbacks<T> {
             dll,
             dll_watcher,
             dll_change_channel: rx,
-            i,
+            flipflop,
         }
     }
 
@@ -132,16 +133,15 @@ impl<T> DllCallbacks<T> {
     ///
     /// keep game state
     pub fn hot_reload(&mut self) {
-        self.i += 1;
-        self.dll = load_dll(self.i);
+        self.flipflop = !self.flipflop;
+        self.dll = load_dll(self.flipflop);
     }
 
     /// reload dll file
     ///
     /// reset game state
     pub fn hot_restart(&mut self) {
-        self.i += 1;
-        self.dll = load_dll(self.i);
+        self.hot_reload();
         self.callbacks = self.dll.new();
     }
 }
